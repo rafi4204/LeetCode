cc7632baf2e6105fe1f2f35b69fc4058de7f0a0f
rafi <muhammad.farook@bs-23.net>
2021-01-30 21:34:05 +0600

    Minimize Deviation in Array

---
+++ src/januray2021Challange/Minimize Deviation in Array.kt
@@ -0,0 +1,27 @@
+package januray2021Challange
+
+import sun.awt.geom.Crossings
+import java.util.*
+
+class Solution7 {
+    fun minimumDeviation(nums: IntArray): Int {
+        val treeSet  = TreeSet<Int>()
+        var md = 0
+        nums.forEach {
+            if(it%2==0)
+                treeSet.add(it)
+            else
+                treeSet.add(it*2)
+        }
+        md = treeSet.last()-treeSet.first()
+
+        while(treeSet.size>0 && treeSet.last()%2==0){
+            md= md.coerceAtMost(treeSet.last() - treeSet.first())
+            val last = treeSet.last()
+            treeSet.remove(last)
+            treeSet.add(last/2)
+
+        }
+        return md
+    }
+}
