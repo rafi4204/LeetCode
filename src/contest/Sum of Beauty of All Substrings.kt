a7efe81e8fe4b9c1c97ceb92b194f080405ef03e
rafi <muhammad.farook@bs-23.net>
2021-03-08 00:30:23 +0600

    Sum of Beauty

--- src/contest/Sum of Beauty of All Substrings.kt
+++ src/contest/Sum of Beauty of All Substrings.kt
@@ -4,32 +4,29 @@ class Solution6 {
     fun beautySum(s: String): Int {
         var result = 0
         for (i in s.indices) {
-            for (len in i+1..s.length ) {
-                result += countBeauty(s.substring(i, len))
+            val map = hashMapOf<Char, Int>()
+
+            for (len in i + 1..s.length) {
+                if (map[s[len]] == null)
+                    map[s[len]] = 1
+                else
+                    map[s[len]] = map[s[len]]!! + 1
+                if (map.size == 1 || map.isEmpty())
+                    continue
+                else {
+                    val temp = map.values.sortedDescending()
+                    result += temp[0] - temp[temp.size - 1]
+                }
             }
+
+
         }
         return result
 
     }
-
-    fun countBeauty(s: String): Int {
-        val map = hashMapOf<Char, Int>()
-        s.forEach {
-            if (map[it] == null)
-                map[it] = 1
-            else
-                map[it] = map[it]!! + 1
-        }
-        if (map.size == 1 || map.isEmpty())
-            return 0
-        else {
-            val temp = map.values.sortedDescending()
-            return temp[0] - temp[temp.size - 1]
-        }
-    }
 }
 
-fun main(){
+fun main() {
     val sol = Solution6()
     print(sol.beautySum("blwdfjnhvabjuhvzawmkkfvajucwzfknamfhchfcgpbjtlpkaftjebhbyhyorhvtrmrwxvbmipijdxpegmvwpgrlwdkydhwziaveadbkjpmqkyaeuhgsvtyxhfbngklmnlhcpftgbyuybgyobeyuhcgjwpjcxhvlhhggocprrmaxmjemotstvrlxauhguyrfyqyicsybanavfmymmajbqshtjpaqoervtlfutxwuwrnlbzgzqipnwflkzcbitbkpubaukvhwfjuzckgnbsnqqgkmhsexapmdidcxbfgbrmilsgrfqmrfcinlvgcgopazhxwpsrvxelenaxaskmdqwjjivmzijeupkwqfotojupxfxgtdgqschlcobevepzopsxvotvoloyqlzmyvhjduiwkuptkhzutudmqojjiyfitvlbyime"))
 }
