ef61dc4a0879ccc3b76b8e2c522ad193133f605b
rafi <muhammad.farook@bs-23.net>
2021-04-16 16:33:56 +0600

    Remove All Adjacent Duplicates in String II.kt

---
+++ src/aprilChallange2021/Remove All Adjacent Duplicates in String II.kt
@@ -0,0 +1,25 @@
+package aprilChallange2021
+
+import java.util.*
+
+class Solution9 {
+    fun removeDuplicates(s: String, k: Int): String {
+        val stk = Stack<Pair<Char, Int>>()
+        var res = arrayListOf<Char>()
+        stk.push(Pair('#', 1))
+        stk.push(Pair(s[0], 1))
+        for (i in 1 until s.length) {
+            stk.push(Pair(s[i], if (s[i] == stk.peek().first) stk.peek().second + 1 else 1))
+            if (stk.peek().second == k) {
+                for(j in 1..k)
+                    stk.pop()
+
+            }
+        }
+
+        for(i in 1 until stk.size) {
+            res.add(stk[i].first)
+        }
+        return res.toCharArray().joinToString("")
+    }
+}
