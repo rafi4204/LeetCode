fe4952399e7756d0cfbeed6feb55aa2ffba9b041
rafi <muhammad.farook@bs-23.net>
2021-05-12 17:59:18 +0600

    Range Sum Query 2D - Immutable.kt

---
+++ src/mayChallange2021/Range Sum Query 2D - Immutable.kt
@@ -0,0 +1,14 @@
+package mayChallange2021
+
+class NumMatrix(val matrix: Array<IntArray>) {
+
+    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int {
+        var sum:Long = 0
+        for(i in row1..row2)
+            for(j in col1..col2)
+                sum+=matrix[i][j]
+
+        return sum.toInt()
+    }
+
+}
