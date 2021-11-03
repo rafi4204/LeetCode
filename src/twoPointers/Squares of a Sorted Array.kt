38fb8122e59025658baabf0f3a1d9c9f4212fae3
rafi <muhammad.farook@brainstation-23.com>
2021-11-03 23:27:57 +0600

    Surrounded Regions.kt

--- src/twoPointers/Squares of a Sorted Array.kt
+++ src/twoPointers/Squares of a Sorted Array.kt
@@ -1,2 +1,21 @@
 package twoPointers
 
+class Solution {
+    fun sortedSquares(nums: IntArray): IntArray {
+        var s = 0
+        var e = nums.size - 1
+        val list = IntArray(nums.size)
+        var c = nums.size - 1
+        while (s <= e && c >= 0) {
+            if (nums[s] * nums[s] >= nums[e] * nums[e]) {
+                list[c--] = nums[s] * nums[s]
+                s++
+            } else {
+                list[c--] = nums[e] * nums[e]
+                e--
+            }
+
+        }
+        return list
+    }
+}
