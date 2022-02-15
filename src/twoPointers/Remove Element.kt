3778a65222730113d7e02d93fc651419d4e6ea0b
rafi <rafiwilder4204@gmail.com>
2022-02-16 01:24:58 +0600

    Smallest String With Swaps.kt

---
+++ src/twoPointers/Remove Element.kt
@@ -0,0 +1,13 @@
+package twoPointers
+
+class Solution12 {
+    fun removeElement(nums: IntArray, `val`: Int): Int {
+      var j = 0
+        for(i in nums.indices){
+            if(nums[i]!=`val`) {
+                nums[j++] = nums[i]
+            }
+        }
+        return j
+    }
+}
