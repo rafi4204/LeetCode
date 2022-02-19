966a10f8db348085c8e833ff60bc4898055832e4
rafi <rafiwilder4204@gmail.com>
2022-02-19 10:50:36 +0600

    Smallest String With Swaps.kt

--- src/februaryChallange2022/Remove K Digits.kt
+++ src/februaryChallange2022/Remove K Digits.kt
@@ -21,7 +21,8 @@ class Solution3 {
         while (!stk.empty() && k > 0) {
             stk.pop()
             k--
-        }fun removeKdigits(num: String, kk: Int): String {
+        }
+        fun removeKdigits(num: String, kk: Int): String {
             val stk = Stack<Char>()
             var k = kk
             if(k == num.length)
