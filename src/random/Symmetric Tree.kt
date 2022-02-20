eed066a99fec495dc8eb5034e35a1eaf3381c6a7
rafi <rafiwilder4204@gmail.com>
2022-02-21 00:17:23 +0600

    Smallest String With Swaps.kt

--- src/random/Symmetric Tree.kt
+++ src/random/Symmetric Tree.kt
@@ -1,5 +1,6 @@
 package random
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import februaryChallange2021.TreeNode
 
 /**
@@ -14,6 +15,19 @@ import februaryChallange2021.TreeNode
  */
 class Solution {
     fun isSymmetric(root: TreeNode?): Boolean {
-       
+        if (root == null) return true
+        return isSymmetric(root.left, root.right)
+    }
+
+    private fun isSymmetric(root1: TreeNode?, root2: TreeNode?): Boolean {
+        if (root1 == null || root2 == null) {
+            return root1 == root2
+        }
+
+        return root1.`val` == root2.`val` && isSymmetric(root1.left, root2.right) && isSymmetric(
+            root1.right,
+            root2.left
+        )
+
     }
 }
