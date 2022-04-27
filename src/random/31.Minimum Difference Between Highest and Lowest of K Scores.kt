4ce8185c2d2496803bde7924fd2d521f72b97f40
rafi <rafiwilder4204@gmail.com>
2022-04-28 01:25:06 +0600

    13.Min Cost to Connect All Points.kt

--- src/random/31.Minimum Difference Between Highest and Lowest of K Scores.kt
+++ src/random/31.Minimum Difference Between Highest and Lowest of K Scores.kt
@@ -1,2 +1,17 @@
 package random
 
+class Solution31 {
+    fun minimumDifference(nums: IntArray, k: Int): Int {
+        nums.sort()
+        if (k == 1 || nums.isEmpty() || nums.size == 1) return 0
+        var min = Int.MAX_VALUE
+        var i = 0
+        var j = 0
+        while (i <= nums.size - k) {
+            if (Math.abs(nums[i] - nums[i + k - 1]) < min)
+                min = Math.abs(nums[i] - nums[i + k - 1])
+            i++
+        }
+        return min
+    }
+}
