c92d3344ba372d77450a7db3242e73e766942b78
rafi <rafiwilder4204@gmail.com>
2022-05-09 19:47:16 +0600

    35. Number of Substrings

--- src/random/43.Maximum Number of Occurrences of a Substring.kt
+++ src/random/43.Maximum Number of Occurrences of a Substring.kt
@@ -1,4 +1,42 @@
 package random
 
+import java.util.*
 
 
+class Solution43 {
+    fun maxFreq(s: String, maxLetters: Int, minSize: Int, maxSize: Int): Int {
+        var i = 0
+        var uniqChar = 0
+        var mx = 0
+        val set = hashMapOf<Char, Int>()
+        val map = hashMapOf<String, Int>()
+        var j = 0
+        while (j in s.indices) {
+            set[s[j]] = set.getOrDefault(s[j], 0) + 1
+            if (set[s[j]] == 1)
+                uniqChar++
+            while (j - i + 1 > minSize) {
+
+                set[s[i]] = set[s[i]]!! - 1
+                if (set[s[i]] == 0)
+                    uniqChar--
+                i++
+
+            }
+            if (uniqChar <= maxLetters)
+                map[s.substring(i, j + 1)] = map.getOrDefault(s.substring(i, j + 1), 0) + 1
+            j++
+        }
+        map.values.forEach {
+            mx = mx.coerceAtLeast(it)
+        }
+        return mx
+    }
+
+
+}
+
+fun main() {
+    val sol = Solution43()
+    sol.maxFreq("aababcaab", 2, 3, 4)
+}
