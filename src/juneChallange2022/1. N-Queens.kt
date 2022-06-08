73023bed709537fc0082daf62bd64e5d793b7d87
rafi <rafiwilder4204@gmail.com>
2022-06-08 16:13:46 +0600

    35. Number of Substrings

--- src/juneChallange2022/1. N-Queens.kt
+++ src/juneChallange2022/1. N-Queens.kt
@@ -1,30 +1,20 @@
 package juneChallange2022
 
 class Solution1 {
-    fun solveNQueens(n: Int): List<List<String>> {
+    var ans = 0
+    fun totalNQueens(n: Int): Int{
         val res = ArrayList<ArrayList<String>>(n)
         val map = hashMapOf<Pair<Int, Int>, Int>()
 
         rec( 0, res, map, n)
-        return res
+        return ans
     }
 
     private fun rec(
          c: Int, res: ArrayList<ArrayList<String>>, map: HashMap<Pair<Int, Int>, Int>, n: Int
     ) {
         if (c == n) {
-            val list = arrayListOf<String>()
-            for (i in 0 until n) {
-                var row = ""
-                for (j in 0 until n) {
-                    row += if (map[Pair(i, j)] != null)
-                        "Q"
-                    else
-                        "."
-                }
-                list.add(row)
-            }
-            res.add(list)
+            ans++
         }
         for (i in 0 until n) {
             if (check(i, c, map)) {
